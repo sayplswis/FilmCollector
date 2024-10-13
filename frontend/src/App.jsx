6abf83c8108dd3a5644ec11fdb7523a083fe4b6c
sayplswis <sayplswis@users.noreply.github.com>
2024-10-13 21:50:57 +0700

    Initial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Button, Input, Textarea } from '@chakra-ui/react'
+import SideMenu from './components/SideMenu'
+
+function App() {
+  return (
+<div>
+  <SideMenu/>
+  <h1>Hello</h1>
+</div>
+  )
+}
+
+export default App
